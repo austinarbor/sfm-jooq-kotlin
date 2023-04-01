916def523730e94f759241c761586daa2285235f
Austin Arbor <aarbor989@gmail.com>
2023-04-01 17:19:03 -0400

    add base project structure

---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    kotlin("jvm") version "1.8.20"
+    `java-library`
+}
+
+group = "dev.aga"
+version = "0.0.1"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("reflect"))
+
+    compileOnly("org.simpleflatmapper:sfm-jooq:8.2.3")
+    compileOnly("org.jooq:jooq-kotlin:3.18.2")
+
+    testImplementation(platform("org.junit:junit-bom:5.9.2"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.assertj:assertj-core:3.24.2")
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+tasks {
+    test {
+        useJUnitPlatform()
+    }
+}
