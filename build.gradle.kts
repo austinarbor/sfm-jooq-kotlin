d2f81b02d1d50dfe03408ddfb38daff1105e4ac6
Austin Arbor <aarbor989@gmail.com>
2023-04-01 19:04:05 -0400

    dedupe versions in build file

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,17 +10,25 @@ repositories {
     mavenCentral()
 }
 
+// this is ugly, but until https://github.com/dependabot/dependabot-core/issues/1164
+// is resolved, we need to either duplicate the version strings or use something
+// like this
+val junitVersion by extra("5.9.2")
+val sfmVersion by extra("8.2.3")
+val jooqVersion by extra("3.18.2")
+val assertJVersion by extra("3.24.2")
+
 dependencies {
     implementation(kotlin("reflect"))
 
-    compileOnly("org.simpleflatmapper:sfm-jooq:8.2.3")
-    compileOnly("org.jooq:jooq-kotlin:3.18.2")
+    compileOnly("org.simpleflatmapper:sfm-jooq:$sfmVersion")
+    compileOnly("org.jooq:jooq-kotlin:$jooqVersion")
 
-    testImplementation("org.simpleflatmapper:sfm-jooq:8.2.3")
+    testImplementation("org.simpleflatmapper:sfm-jooq:$sfmVersion")
 
-    testImplementation(platform("org.junit:junit-bom:5.9.2"))
+    testImplementation(platform("org.junit:junit-bom:$junitVersion"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    testImplementation("org.assertj:assertj-core:3.24.2")
+    testImplementation("org.assertj:assertj-core:$assertJVersion")
 }
 
 kotlin {
